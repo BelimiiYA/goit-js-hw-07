14ad349f9befa77a01a8f4b3df5f03924e53dbc0
BelimiiYA <116880689+BelimiiYA@users.noreply.github.com>
2023-04-21 10:39:14 +0200

    Add files via upload

---
+++ js/01-gallery.js
@@ -0,0 +1,52 @@
+import { galleryItems } from "./gallery-items.js";
+
+const refGallery = document.querySelector(".gallery");
+
+const elements = galleryItems.map((option) => {
+  const elementList = document.createElement("li");
+  elementList.classList.add("gallery__item");
+
+  const elementLink = document.createElement("a");
+  elementLink.classList.add("gallery__link");
+  7;
+  elementLink.href = option.original;
+
+  const elementLinkImg = document.createElement("img");
+  elementLinkImg.classList.add("gallery__image");
+  elementLinkImg.src = option.preview;
+  elementLinkImg.alt = option.description;
+  elementLinkImg.dataset.source = "large-image.jpg";
+
+  elementLink.append(elementLinkImg);
+  elementList.append(elementLink);
+
+  return elementList;
+});
+
+refGallery.append(...elements);
+
+refGallery.addEventListener("click", selectImg);
+
+function selectImg(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const parentImg = evt.target.closest(".gallery__link");
+  const hrefImgOriginal = parentImg.href;
+
+  const instance = basicLightbox.create(`
+    <img src="${hrefImgOriginal}">
+`);
+  instance.show();
+
+  document.addEventListener("keydown", (evt) => {
+    const keyName = evt.key;
+    // console.log(evt.key);
+    if (keyName !== "Escape") {
+      return;
+    }
+    instance.close();
+  });
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,22 @@
+import { galleryItems } from "./gallery-items.js";
+
+const paletteGallery = document.querySelector(".gallery");
+const galleryCardMarkup = createGalleryCardMarkup(galleryItems);
+
+paletteGallery.insertAdjacentHTML("beforeend", galleryCardMarkup);
+
+function createGalleryCardMarkup(galleryItems) {
+  const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" title="${description}" alt="${description}" />
+        </a>
+        </li>
+    `;
+  });
+
+  return markup.join("");
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {});
